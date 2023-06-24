dae176959007dc5c46478dc68df32ec2bacba99a
Jeggam-Mahesh <jeggammahesh15720@gmail.com>
2023-06-25 01:35:52 +0530

    changes of cards

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
- var temp=document.getElementById('popbox')
- let emptyvar=document.getElementById('empty')
- let fg=false;
- function showpopup(){
-  emptyvar.style.display='none';
+var temp=document.getElementById('popbox')
+let emptyvar=document.getElementById('empty')
+let removetaskpopup=document.getElementById('rpopup')
+let tasklist=document.getElementById('head')
+let specialcard=document.getElementById('specialcard');
+let firstchild=document.getElementById('firstchild');
+let inlinediv=document.getElementById('inlinediv');
+function showpopup(){
  
-  if(fg){
-  temp.classList.remove('showpop')
-  fg=false;
-  }
-  else{
-    fg=true
-  temp.classList.add('showpop')  }
- }
+ temp.classList.add('showpop')
+}
 
- function hide(){
+function hide(){
+ temp.classList.remove ('showpop')
+}
+
+//*******************Adding new caard  ***************
+let container=document.getElementById('maincontainer');
+// let specialCrd=document.getElementById('specialcard');
+let cnt=0;
+let cnt2=0;
+let sample;
+let ceeeer;
+let cardd;
+let flag=false;
+let map=new Map();
+       
+function newcard(){
+  cnt++; 
+  cnt2++;
+   container.style.display='flex';
   temp.classList.remove ('showpop')
- }
-
- //*******************Adding new caard  ***************
- let container=document.getElementById('maincontainer');
- let specialCrd=document.getElementById('specialcard');
- var cnt=0;
- let map=new Map();
-
- function newcard(){
-   temp.classList.remove ('showpop')
 let card=document.createElement('div');
 card.setAttribute('id',`card${cnt}`)
+
+let tripname=document.createElement('h3')
 let hr=document.createElement('hr');
- let tripname=document.createElement('h3')
- tripname.setAttribute('id',`trip${cnt}`);
-   
+tripname.setAttribute('id',`trip${cnt}`);
+  
 //  console.log(tripname.getAttribute('id','value'))
- let inputtext=document.getElementById('tripname');
-    tripname.innerHTML=`${inputtext.value}`;
- let ad_btn=document.createElement('button');
- let del_btn=document.createElement('button');
- let taskdiv=document.createElement('div'); 
+let inputtext=document.getElementById('tripname');
+   tripname.innerHTML=`${inputtext.value}`;
+let ad_btn=document.createElement('button');
+let del_btn=document.createElement('button');
+let taskdiv=document.createElement('div'); 
 
-  taskdiv.setAttribute('id',`taskdiv${cnt}`);
-  ad_btn.setAttribute('id',`wrt_task${cnt}`);
-  //storing in map
-  map.set(`wrt_task${cnt}`,`taskdiv${cnt}`);
-  console.log(map);
+ taskdiv.setAttribute('id',`taskdiv${cnt}`);
+ ad_btn.setAttribute('id',`wrt_task${cnt}`);
+ //storing in map
+ map.set(`wrt_task${cnt}`,`taskdiv${cnt}`);
+ console.log(map);
 
-  taskdiv.setAttribute('class','taskdivision')
-  ad_btn.setAttribute('class','wrt_task')
+ taskdiv.setAttribute('class','taskdivision')
+ ad_btn.setAttribute('class','wrt_task')
 del_btn.setAttribute('id','deleticon');
- 
-
 
 let deleticon=document.createElement('img');
 let wrt_task=document.createElement('img');
@@ -70,65 +74,115 @@ card.appendChild(taskdiv)
 card.appendChild(del_btn);
 card.appendChild(ad_btn);
 
+//deleting a card.*********** 
+del_btn.addEventListener('click',()=>{
+ card.remove();
+ cnt2--;
+ if(cnt2<1){
+  emptyvar.style.display='block';
+  }
+})
 
-//deleting a card. 
- del_btn.addEventListener('click',()=>{
-  card.remove();
- })
-
-ad_btn.setAttribute('onclick','show_taskpopup()')
-
-let add_item_btn=document.getElementById('additems');
-add_item_btn.addEventListener('click',()=>{
-
-   let inputitem=document.getElementById('itemname');
-   let tasktodo=document.createElement('p');
-   tasktodo.innerHTML=`${inputitem.value}`;
-   let sample=map.get(`wrt_task${cnt-1}`);
-  //  console.log(cnt);
-  //  console.log(sample);
-  let taskwrite=document.getElementById(`${sample}`)
-  taskwrite.appendChild(tasktodo);
-  removetaskpopup.style.display="none";
-  container.style.display="flex";
-  })
-
-  let tr=document.getElementById(`trip0`);
-  // console.log(tr.innerHTML);
+//SHOW TASK POPUP.
+ad_btn.addEventListener('click',()=>{
+ sample=ad_btn.getAttribute('id','value')
+   ceeeer=map.get(sample)
+  show_taskpopup();
   
-  tr.addEventListener('click',()=>{
-    console.log("hello");
+})
+
+// let add_item_btn=document.getElementById('additems');
+
+// ad_btn.setAttribute('onclick','show_taskpopup()')
+// if(cnt>0){
+
+   cardd = document.getElementById('card1');
+  console.log(cardd)
    
-  let carddd=document.getElementById('card0');
-  specialCrd.appendChild(carddd);
-    container.style.display="none";
-    specialCrd.style.display="block";
-  })
+   console.log(specialcard)
+    let tempp=document.getElementById('trip1');
+    console.log(tempp)
+    tempp.addEventListener('click',()=>{
+     flag=false;
+     tasklist.style.display='none';
+      container.style.display="none";
+    specialcard.style.display='block'
+      inlinediv.appendChild(cardd);
+    })
+// }
+if(cnt2>0){
+  emptyvar.style.display='none';
+  }
 
-  cnt++; 
- }
- function spcl_card_close(){
-  specialCrd.style.display="none";
-  container.style.display="flex"
- }
-// let aas=document.getElementById('trip1')
+}
+console.log(cnt2)
 
 
- //function showtask popup and hidden
 
- let removetaskpopup=document.getElementById('rpopup')
+function spcl_card_close(){
+  tasklist.style.display='block';
+ specialcard.style.display="none";
+ container.style.display="flex"
+ inlinediv.removeChild(cardd);
+ firstchild.appendChild(cardd);
+ firstchild.style.display='block'
+}
+
+//function showtask popup and hidden
+
+
 function show_taskpopup(c){
 
-  removetaskpopup.style.display="block";  
-  container.style.display="none";
+ removetaskpopup.style.display="block";  
+ container.style.display="none";
 
- }
- function hiden(){
-  removetaskpopup.style.display="none";
-  container.style.display="flex"
 }
-  //////////
+function hiden(){
+  let container=document.getElementById('maincontainer');
+ removetaskpopup.style.display="none";
+ container.style.display="flex";
+}
+//FUNCTION ADD SUBTASKS
+function addtodo(){
+  removetaskpopup.style.display="none";
+  // if(flag){
+  //   container.style.display='flex';
+  // }
+  // else{
+  // container.style.display='none';
+  
+  // }
+  container.style.display='flex';
+  let append_to_div=document.getElementById(`${ceeeer}`);
+  let inputitem=document.getElementById('itemname');
+  let tasktodo=document.createElement('h4');
+  
+  let done_button=document.createElement('button');
+  done_button.setAttribute('class','done_button');
+  done_button.innerHTML="Done"
+  tasktodo.innerHTML=`${inputitem.value}`;
+  tasktodo.appendChild(done_button);
+  
+  append_to_div.appendChild(tasktodo);
+  //MARKING TASKS DONE
+let toggle=true;
+  done_button.addEventListener('click',()=>{
+    if(toggle){
+    tasktodo.classList.add('marktext')
+    toggle=false;}
+    else{
+      tasktodo.classList.remove('marktext')
+      toggle=true;
+    }
+  })
+}
+ 
+//  console.log(cardd);
+//  
+function back(){
+  specialcard.style.display='none';
+  container.style.display='flex';
+    flag=true;
+}
 
-
-  ////special card function
-
+ 
